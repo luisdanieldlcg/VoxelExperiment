2b0930fe4a114cc0f71b53de5e384a2d80bdab9a
DanikingRD <danikingrd@gmail.com>
2024-01-22 22:16:58 -0400

    feat: render test triangle

--- assets/shaders/terrain.wgsl
+++ assets/shaders/terrain.wgsl
@@ -1,114 +1,24 @@
-struct Globals {
+struct Uniforms {
     view: mat4x4<f32>,
     proj: mat4x4<f32>,
-    sun_pos: vec3<f32>,
-    enable_lighting: u32,
-    atlas_size: u32,
-    tile_size: u32,
-};
-
-@group(0) @binding(0)
-var<uniform> globals: Globals;
-
-@group(1) @binding(0)
-var<uniform> chunk_pos: vec2<i32>;
-
-struct VertexInput {
-    @builtin(vertex_index) v_index: u32,
-    @location(0) data: u32,
-};
-
-struct VertexOutput {
-    @builtin(position) vertices: vec4<f32>,
-    @location(0) tex_coords: vec2<f32>,
-    @location(1) normal: vec3<i32>,
-    @location(2) local_pos: vec3<f32>,
-};
-
-fn calculate_texture_coordinates(v_index: u32, data: u32) -> vec2<f32> {
-    // Calculate the texture coordinates based on the texture id
-    // let texture_id = data & 0x1FFFu; // mask 13 bits
-    // mask 10 bits
-    let texture_id = data & 0x3FFu;
-    let texture_width = globals.tile_size;
-    let texture_height = globals.tile_size;
-    // number of columns in the atlas
-    let cols = globals.atlas_size / texture_width; 
-    let pixel_x = f32((texture_id % cols) * texture_width);
-    let pixel_y = f32((texture_id / cols) * texture_height);
-
-    switch (v_index % 4u) {
-          case 0u: {
-              return vec2<f32>(pixel_x / f32(globals.atlas_size), (pixel_y + f32(texture_height)) / f32(globals.atlas_size));
-          }
-          case 1u: {
-              return vec2<f32>((pixel_x + f32(texture_width)) / f32(globals.atlas_size), (pixel_y + f32(texture_height)) / f32(globals.atlas_size));
-          }
-          case 2u: {
-              return vec2<f32>((pixel_x + f32(texture_width)) / f32(globals.atlas_size), pixel_y / f32(globals.atlas_size));
-          }
-          case 3u: {
-              return vec2<f32>(pixel_x / f32(globals.atlas_size), pixel_y  / f32(globals.atlas_size));
-          }
-          default: {
-              return vec2<f32>(0.0, 0.0);
-          }
-      }
 }
 
-fn unpack_vertex_data(data: u32) -> vec3<f32> {
-    let x = (data >> 27u) & 0x1Fu;
-    let y = (data >> 18u) & 0x1FFu;
-    let z = (data >> 13u) & 0x1Fu;
-    return vec3<f32>(f32(x), f32(y), f32(z));
+struct VertexIn {
+    @location(0) vertex_pos: vec3<f32>,
 }
 
-fn unpack_normals(data: u32) -> vec3<i32> {
-    let x = (data >> 12u) & 0x1u;
-    let y = (data >> 11u) & 0x1u;
-    let z = (data >> 10u) & 0x1u;
-    // map range [0, 1] to [-1, 1]
-    return vec3<i32>(
-        i32(x) * 2 - 1,
-        i32(y) * 2 - 1,
-        i32(z) * 2 - 1
-    );
+struct VertexOut {
+    @builtin(position) vertex_pos: vec4<f32>,
 }
 
 @vertex
-fn vs_main(input: VertexInput) -> VertexOutput {
-    var output: VertexOutput;
-
-    let local_pos = unpack_vertex_data(input.data);
-    let world_pos = vec3<f32>(
-        f32(chunk_pos.x) * 16.0 + local_pos.x,
-        local_pos.y,
-        f32(chunk_pos.y) * 16.0 + local_pos.z
-    );
-    output.vertices = globals.proj * globals.view * vec4<f32>(world_pos, 1.0);
-    output.tex_coords = calculate_texture_coordinates(input.v_index, input.data);
-    output.normal = unpack_normals(input.data);
-    output.local_pos = local_pos;
-    return output;
+fn vs_main(in: VertexIn) -> VertexOut{
+    var out: VertexOut;
+    out.vertex_pos = vec4<f32>(in.vertex_pos, 1.0);
+    return out;
 }
 
-@group(0) @binding(1)
-var texture: texture_2d<f32>;
-@group(0) @binding(2)
-var texture_sampler: sampler;
-
 @fragment
-fn fs_main(input: VertexOutput) -> @location(0) vec4<f32> {
-    let obj_color = textureSample(texture, texture_sampler, input.tex_coords);
-    if (globals.enable_lighting == 0u) {
-        return obj_color;
-    }
-    let ambient_factor = 0.36;
-    let light_color = vec3<f32>(1.0, 1.0, 1.0);
-    let ambient = ambient_factor * light_color;
-    let light_dir = normalize(globals.sun_pos - input.local_pos);
-    let diff = max(dot(vec3<f32>(input.normal), light_dir), 0.0);
-    let diffuse = diff * light_color;
-    let result = (diffuse + ambient) * obj_color.xyz;
-    return vec4<f32>(result, obj_color.w);
+fn fs_main(in: VertexOut) -> @location(0) vec4<f32> {
+    return vec4<f32>(0.4, 0.3, 0.2, 1.0);
 }
--- client/src/renderer/mod.rs
+++ client/src/renderer/mod.rs
@@ -1,13 +1,39 @@
 use std::sync::Arc;
 
+use wgpu::util::DeviceExt;
 use winit::window::Window;
 
+#[repr(C)]
+#[derive(Copy, Clone, Debug, bytemuck::Pod, bytemuck::Zeroable)]
+pub struct Vertex {
+    pub position: [f32; 3],
+}
+
+impl Vertex {
+    pub fn desc<'a>() -> wgpu::VertexBufferLayout<'a> {
+        const ATTRS: [wgpu::VertexAttribute; 1] = wgpu::vertex_attr_array![0 => Float32x3];
+        wgpu::VertexBufferLayout {
+            step_mode: wgpu::VertexStepMode::Vertex,
+            attributes: &ATTRS,
+            array_stride: std::mem::size_of::<Vertex>() as wgpu::BufferAddress,
+        }
+    }
+}
+
+/// Manages the rendering of the application.
 pub struct Renderer {
     window: Arc<Window>,
+    /// Surface on which the renderer will draw.
     surface: wgpu::Surface<'static>,
+    /// The GPU device. Contains functions used for interacting with the GPU.\
     device: wgpu::Device,
+    /// A GPU Queue handle. Used for submitting commands to the GPU.
     queue: wgpu::Queue,
+    /// The surface configuration. This is used for configuring the surface.
     config: wgpu::SurfaceConfiguration,
+    /// The render pipeline configuration.
+    render_pipeline: wgpu::RenderPipeline,
+    vertex_buffer: wgpu::Buffer,
 }
 
 impl Renderer {
@@ -36,10 +62,82 @@ impl Renderer {
             None, // Trace path
         ))
         .unwrap();
-
         let (width, height) = window.inner_size().into();
         let config = surface.get_default_config(&adapter, width, height).unwrap();
         surface.configure(&device, &config);
+
+        let pipeline_layout = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+            label: None,
+            bind_group_layouts: &[],
+            push_constant_ranges: &[],
+        });
+
+        let shader = device.create_shader_module(wgpu::ShaderModuleDescriptor {
+            label: None,
+            source: wgpu::ShaderSource::Wgsl(
+                include_str!("../../../assets/shaders/terrain.wgsl").into(),
+            ),
+        });
+
+        let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+            label: None,
+            layout: Some(&pipeline_layout),
+            vertex: wgpu::VertexState {
+                module: &shader,
+                entry_point: "vs_main",
+                buffers: &[Vertex::desc()],
+            },
+            fragment: Some(wgpu::FragmentState {
+                module: &shader,
+                entry_point: "fs_main",
+                targets: &[Some(wgpu::ColorTargetState {
+                    format: config.format,
+                    blend: Some(wgpu::BlendState::REPLACE),
+                    write_mask: wgpu::ColorWrites::all(),
+                })],
+            }),
+            primitive: wgpu::PrimitiveState {
+                topology: wgpu::PrimitiveTopology::TriangleList,
+                strip_index_format: None,
+                front_face: wgpu::FrontFace::Ccw,
+                cull_mode: Some(wgpu::Face::Back),
+                polygon_mode: wgpu::PolygonMode::Fill,
+                unclipped_depth: false,
+                conservative: false,
+            },
+            // depth_stencil: Some(wgpu::DepthStencilState {
+            //     format: texture::Texture::DEPTH_FORMAT,
+            //     depth_write_enabled: true,
+            //     depth_compare: wgpu::CompareFunction::Less,
+            //     stencil: wgpu::StencilState::default(),
+            //     bias: wgpu::DepthBiasState::default(),
+            // }),
+            depth_stencil: None,
+            multisample: wgpu::MultisampleState {
+                count: 1,
+                mask: !0,
+                alpha_to_coverage_enabled: false,
+            },
+            multiview: None,
+        });
+        let triangle_vertices = [
+            Vertex {
+                position: [0.0, 0.5, 0.0],
+            }, // A
+            Vertex {
+                position: [-0.5, -0.5, 0.0],
+            }, // B
+            Vertex {
+                position: [0.5, -0.5, 0.0],
+            }, // C
+        ];
+        let vertex_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+            label: None,
+            contents: bytemuck::cast_slice(&triangle_vertices),
+            usage: wgpu::BufferUsages::VERTEX,
+        });
+
+        tracing::info!("Renderer initialized.");
 
         Self {
             surface,
@@ -47,10 +145,63 @@ impl Renderer {
             queue,
             config,
             window,
+            render_pipeline,
+            vertex_buffer,
         }
     }
 
-    pub fn resize(&mut self, width: u32, height: u32) {}
+    pub fn resize(&mut self, width: u32, height: u32) {
+        self.config.width = width;
+        self.config.height = height;
+        self.surface.configure(&self.device, &self.config);
+    }
 
-    pub fn render(&mut self) {}
+    pub fn render(&mut self) {
+        let output = match self.surface.get_current_texture() {
+            Ok(t) => t,
+            Err(wgpu::SurfaceError::Lost) => {
+                let (width, height) = self.window.inner_size().into();
+                self.resize(width, height);
+                tracing::warn!("Surface lost, resizing. A frame will be dropped.");
+                return;
+            },
+            Err(e) => panic!("{:#?}", e),
+        };
+
+        let view = output
+            .texture
+            .create_view(&wgpu::TextureViewDescriptor::default());
+
+        let mut encoder = self
+            .device
+            .create_command_encoder(&wgpu::CommandEncoderDescriptor::default());
+
+        {
+            let mut render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+                label: None,
+                color_attachments: &[Some(wgpu::RenderPassColorAttachment {
+                    view: &view,
+                    resolve_target: None,
+                    ops: wgpu::Operations {
+                        load: wgpu::LoadOp::Clear(wgpu::Color {
+                            r: 0.1,
+                            g: 0.2,
+                            b: 0.3,
+                            a: 1.0,
+                        }),
+                        store: wgpu::StoreOp::Store,
+                    },
+                })],
+                depth_stencil_attachment: None,
+                timestamp_writes: None,
+                occlusion_query_set: None,
+            });
+            render_pass.set_pipeline(&self.render_pipeline);
+            render_pass.set_vertex_buffer(0, self.vertex_buffer.slice(..));
+            render_pass.draw(0..3, 0..1);
+        }
+
+        self.queue.submit(Some(encoder.finish()));
+        output.present();
+    }
 }
